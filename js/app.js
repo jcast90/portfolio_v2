ceb109ea3ce58de4fed5dd8ded909a252aac1188
jcast90 <jcast90@me.com>
2016-11-28 07:07:49 -0800

    added functionality
    added arrow animation, some styling to my main message and arrow.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
     $('span.hidden').fadeIn(4000).removeClass('hidden');
-})
+    $(function () {
+        $('#arrow_down').effect('bounce', {
+            times: 10,
+            distance:20,
+        }, 2000);
+        $('#arrow_down').click(function(){
+            $('html,body').animate({
+            scrollTop: $(".header_h2").offset().top},
+            'slow');
+            $('.nav li').setAttribute('style','position:fixed;')
+});
+        
+    });
+});
