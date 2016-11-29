f462032ce69f09f691f84d0a901e70925e51bcc5
Jonathan Lancaster <jcast90@me.com>
2016-11-29 08:04:51 -0800

    updated styling
    added color scheme, improved layout and responsiveness with bootstrap

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
+
 $(document).ready(function () {
     $('span.hidden').fadeIn(4000).removeClass('hidden');
     $(function () {
         $('#arrow_down').effect('bounce', {
             times: 10,
-            distance:20,
+            distance: 20
         }, 2000);
         $('#arrow_down').click(function(){
             $('html,body').animate({
             scrollTop: $(".header_h2").offset().top},
             'slow');
-            $('.nav li').setAttribute('style','position:fixed;')
+           // $('.nav').css('position','fixed');
+
 });
+
         
     });
 });
