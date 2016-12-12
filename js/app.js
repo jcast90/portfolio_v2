96d6c6da919ef3db0b2725db3ae69832e7d4b8db
Jonathan Lancaster <jcast90@me.com>
2016-12-12 07:50:19 -0800

    Update css
    update contact me, modal windows

--- js/app.js
+++ js/app.js
@@ -1,19 +1,15 @@
-
 $(document).ready(function () {
     $('span.hidden').fadeIn(4000).removeClass('hidden');
     $(function () {
         $('#arrow_down').effect('bounce', {
-            times: 10,
-            distance: 20
+            times: 10
+            , distance: 20
         }, 2000);
-        $('#arrow_down').click(function(){
+        $('#arrow_down').click(function () {
             $('html,body').animate({
-            scrollTop: $(".header_h2").offset().top},
-            'slow');
-           // $('.nav').css('position','fixed');
-
-});
-
-        
+                scrollTop: $(".header_h2").offset().top
+            }, 'slow');
+            // $('.nav').css('position','fixed');
+        });
     });
 });
